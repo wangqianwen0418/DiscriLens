e0059c2d09dfe522c14ba6b12b0206fe9e1f288a
wangqianwen0418 <wangqianwen0418@163.com>
2019-01-29 12:48:36 +0800

    fix conflict

--- py/server/api.py
+++ py/server/api.py
@@ -1,6 +1,8 @@
 from flask import request, jsonify, Blueprint, current_app, Response
-from model import generate_samples, generate_model_samples, ModelGene, findKeyAttrs, FindGroups, find_rules
+from model import num2cate, generate_samples, generate_model_samples, ModelGene, findKeyAttrs, FindGroups, get_numAttrs, find_rules
 import pandas as pd
+from model.samples import DataGene
+from model.data_encoder import DataEncoder
 from joblib import dump, load
 
 import json
@@ -19,6 +21,21 @@ def get_dataset(dataset_name):
     df = pd.read_csv(dataset_path, 'r', delimiter=',')
     return df.to_json()
 
+@api.route('/generate_num/<string:dataset_name>', methods=['GET'])
+def get_numData(dataset_name):
+    """Fetch dataset by id"""
+    dataset_path = '../data/{}.csv'.format(dataset_name)
+    data = pd.read_csv(dataset_path)
+    data = data.dropna(how='any')
+    dataGene = DataGene(data, sample_num=3000)
+    new_samples = dataGene.get_samples()
+    #new_samples.to_json('../data/file.json', orient='records', lines=True)
+    ''' 
+    samples_path = os.path.join(cache_path, '{}_samples.csv'.format(dataset_name, model_name))
+    new_samples.to_csv(samples_path, index=False)
+    new_samples.to_json('./new.json', orient='records')
+    '''
+    return new_samples.to_json()
 
 
 @api.route('/samples', methods=['GET'])
@@ -82,12 +99,11 @@ def get_groups():
 
     dataset_name = request.args.get('dataset', None, type=str)
     model_name = request.args.get('model', None, type=str)
-    model_name = '{}_{}'.format(dataset_name, model_name)
-    protect_attr = request.args.get('protect', None, type=str)
-
-    # get training data
-    dataset_path = '../data/{}_clean.csv'.format(dataset_name)
+    protect_attr = request.args.get('protectAttr', None, type=str)
+    # get traiing data
+    dataset_path = '../data/{}.csv'.format(dataset_name)
     data = pd.read_csv(dataset_path)
+    data = num2cate(data)
 
     # get model samples
     sample_path = os.path.join(cache_path, '{}_samples.csv'.format(model_name))
@@ -97,17 +113,16 @@ def get_groups():
     key_attrs = findKeyAttrs(data, protect_attr)
 
     key_vals = {}
+    key_groups = []
     for key_attr in key_attrs:
+        #if(type(model_samples[key_attr][0])!=type(model_samples['id'][0])):
         key_vals[key_attr] = list(set(data[key_attr]))
-
+    
     findGroups = FindGroups(key_vals)
     key_groups = findGroups.locate_items(model_samples, protect_attr)
     
     for i, group in enumerate(key_groups):
         key_groups[i]['items'] = list(map(int, key_groups[i]['items']))
-
-
-    
 
     return_value = {
         'key_attrs': key_attrs,
@@ -123,8 +138,6 @@ def get_groups():
 
 
 
-
-
 @api.route('/key_attrs/<string:dataset_name>', methods=['GET'])
 def get_key_attrs(dataset_name):
     """
@@ -132,7 +145,7 @@ def get_key_attrs(dataset_name):
     E.g.: /api/key_attrs/credit
     """
     protect_attr = ''
-    dataset_path = '../data/{}_clean.csv'.format(dataset_name)
+    dataset_path = '../data/{}.csv'.format(dataset_name)
     data = pd.read_csv(dataset_path)
     key_attrs = findKeyAttrs(data, protect_attr)
 
