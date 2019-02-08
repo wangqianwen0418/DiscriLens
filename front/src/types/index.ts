e7a5c78558ce72b9eade50db894cb2e94d30067e
TOnyXuQAQ <tonyxuhit@gmail.com>
2019-02-08 18:40:31 +0800

    commit

--- front/src/types/index.ts
+++ front/src/types/index.ts
@@ -17,12 +17,13 @@ export enum Status {
 }
 
 export interface StoreState{
-    key_attrs: string[],
-    samples: DataItem[],
-    rules: DataItem[],
-    key_groups: KeyGroup[],
-    fetch_samples_status: Status,
-    fetch_groups_status: Status,
-    thr_rules: number[],
-    protected_attr: string
+    key_attrs: string[], // record key attrs
+    samples: DataItem[],    // record all samples 
+    rules: DataItem[],  // record all rules
+    key_groups: KeyGroup[], // record all data of key attrs
+    fetch_samples_status: Status,   // loading status
+    fetch_groups_status: Status,    // loadinf status
+    thr_rules: number[],    // slider bar interaction, select threshold of rule filtering
+    protected_attr: string, // record protected attr
+    g_endPos: number[][],   // <g> 's end position, record the position of each attr's corresponding bars
 }
