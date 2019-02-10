28c338bd708d649de91c64fdd3a3179a60150e15
Zhenhua <tonyxuhit@gmail.com>
2019-02-10 15:23:26 +0800

    1

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -14,23 +14,39 @@ import { StoreState, Status} from 'types';
 
 import 'antd/dist/antd.css';
 
-const TEST = false
+const TEST = true
 
-let initState:StoreState = {
-  key_attrs: [],
-  key_groups: [],
-  samples: [],
-  rules: [],
-  protected_attr: '',
-  fetch_samples_status: Status.INACTIVE,
-  fetch_groups_status: Status.INACTIVE,
-  thr_rules:[-0.1,0.1],
-  drag_array: [],
-}
-
+let initState:StoreState
  if (TEST){
- 
- }
+    let jsonGroups = require('./testdata/groups_dataTest_knn.json'), 
+    jsonSamples = require('./testdata/dataTest_knn_samples.json'),
+    jsonRule = require('./testdata/dataTest_knn_rules.json')
+    initState = {
+      key_attrs: ["hours_per_week", 
+      "relationship", 
+      "education_num"],
+      key_groups: jsonGroups,
+      samples: jsonSamples,
+      rules: jsonRule,
+      protected_attr: 'sex',
+      fetch_samples_status: Status.COMPLETE,
+      fetch_groups_status: Status.COMPLETE,
+      thr_rules:[-0.1,0.1],
+      drag_array: [],
+  }
+}else{
+  initState = {
+    key_attrs: [],
+    key_groups: [],
+    samples: [],
+    rules: [],
+    protected_attr: '',
+    fetch_samples_status: Status.COMPLETE,
+    fetch_groups_status: Status.COMPLETE,
+    thr_rules:[-0.1,0.1],
+    drag_array: [],
+}
+}
 
 const store = createStore(
   rootReducer,
