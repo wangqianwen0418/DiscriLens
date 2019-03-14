73f6b055d11ed6e6751f7b3e3f9c26a4af8d5728
wangqianwen0418 <wangqianwen0418@163.com>
2019-03-14 18:32:29 +0800

    update academic data

--- py/server/model/data_encoder.py
+++ py/server/model/data_encoder.py
@@ -50,7 +50,7 @@ def interval2string(inter):
     if (inter[0]==-math.inf):
         return 'x<{}'.format(int(inter[1]))
     elif (inter[1]==math.inf):
-        return 'x<{}'.format(int(inter[0]))
+        return 'x>{}'.format(int(inter[0]))
     else:
         return '{}<x<{}'.format(int(inter[0]), int(inter[1]))
 
@@ -67,6 +67,7 @@ def num2cate_fit(df, min=4):
     Y = df.iloc[:, -1].values
     continuous_features =df.iloc[:, :-1].select_dtypes(include=['int64','float64']).columns.tolist()
     continuous_features.sort() # ensoure the features order between fit and transform
+    print('fit', continuous_features)
     X = df[continuous_features].values
     mdlp = MDLP(min_depth=min)
     mdlp.fit(X, Y) # X, Y should be numpy array
@@ -87,6 +88,7 @@ def num2cate_transform(df, mdlp):
     else:
         continuous_features =df.select_dtypes(include=['int64','float64']).columns.tolist()
     continuous_features.sort() # ensoure the features order between fit and transform
+    print('transform', continuous_features)
     X = df[continuous_features].values
     conv_X = mdlp.transform(X)
 
